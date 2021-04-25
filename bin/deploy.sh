6d8ee6e72b7e51fbb62d3edec010fa0b6d7d7e92
manhtai <vo@manhtai.com>
2021-04-25 20:23:41 +0700

    Not kill all gunicorn

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,6 @@ poetry run ./manage.py migrate
 poetry run ./manage.py collectstatic --noinput
 
 echo "Restarting gunicorn..."
-killall gunicorn || true
+lsof -ti :8001 | xargs kill || true
 poetry run gunicorn --workers 1 --bind=0.0.0.0:8001 apps.wsgi:application --timeout 30 --daemon
 echo "Gunicorn restarted!"
